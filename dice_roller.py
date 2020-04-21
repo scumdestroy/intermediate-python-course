1d68cac47b74cb432534d5635b29750c7042ec30
Jan Slezak <scumdestroy@gmail.com>
2020-04-21 14:00:58 -0700

    tutty completey

--- dice_roller.py
+++ dice_roller.py
@@ -1,15 +1,18 @@
 import random
 
 
-dice_rolls = 2
+dice_rolls = int(input("how many dice ye rollin?? ummm....? "))
+dice_size = int(input('How many sides on them lucky boulders? well...? '))
 dice_sum = 0
 
-for i in range(0,dice_rolls):
+for i in range(1,dice_rolls):
   roll = random.randint(1,6)
   dice_sum += roll
-  print('You rolled a die')
-  print(f'You rolled a {roll}')
-  print(f'You rolled a total of {dice_sum}')
+  if roll == 1:
+  	print('piss poor job mate, a bloody "one"')
+  elif roll == 6:
+  	print('bloody brilliant that roll, six six six the name')
+  else:
+  	print(f'You rolled a {roll}')
 
-if __name__== "__main__":
-  main()
+print(f'You rolled a total of {dice_sum}')
