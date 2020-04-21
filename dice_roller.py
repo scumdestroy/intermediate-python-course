1e92bfe0024168e732466e590061bde8de3770b9
Jan Slezak <scumdestroy@gmail.com>
2020-04-21 13:54:58 -0700

    now rollin rollin rollin x dice or die or douse

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,15 @@
-def main():
+import random
+
+
+dice_rolls = 2
+dice_sum = 0
+
+for i in range(0,dice_rolls):
+  roll = random.randint(1,6)
+  dice_sum += roll
   print('You rolled a die')
+  print(f'You rolled a {roll}')
+  print(f'You rolled a total of {dice_sum}')
 
 if __name__== "__main__":
   main()
